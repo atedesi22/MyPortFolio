3cf7a381ed8144df350cb182b92384ce33391d1b
atedesi22 <paulemmanuelbohole5@gmail.com>
2026-01-27 16:01:19 +0100

    expertise + challenges + shop+ contact +cv

--- vite.config.js
+++ vite.config.js
@@ -11,28 +11,28 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Atedesi Bohole Portfolio',
-        short_name: 'AtedesiDev',
-        display: "standalone", // TRÈS IMPORTANT pour que le bouton apparaisse
-        start_url: "/",
-        description: 'Portfolio Fullstack de Atedesi Bohole Paul Emmanuel',
-        theme_color: '#0f172a', // Ton bleu foncé/noir
-        icons: [
-          {
-            src: 'favicon-96x96.png', // Tu devras mettre ces images dans /public
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'favicon-96x96.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   manifest: {
+    //     name: 'Atedesi Bohole Portfolio',
+    //     short_name: 'AtedesiDev',
+    //     display: "standalone", // TRÈS IMPORTANT pour que le bouton apparaisse
+    //     start_url: "/",
+    //     description: 'Portfolio Fullstack de Atedesi Bohole Paul Emmanuel',
+    //     theme_color: '#0f172a', // Ton bleu foncé/noir
+    //     icons: [
+    //       {
+    //         src: 'favicon-96x96.png', // Tu devras mettre ces images dans /public
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'favicon-96x96.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       }
+    //     ]
+    //   }
+    // })
   ],
 })

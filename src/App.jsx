aac38339a7a4e9e4e04c20d7f78b39aba7df4885
atedesi22 <paulemmanuelbohole5@gmail.com>
2026-01-27 15:08:38 +0100

    expertise + challenges + shop+ contact +cv

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <footer className='bg-slate-950 py-20 border-t border-slate-800'>
           <div className='max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-16'>
             
-            <Contact />
+            <Contact id='contact'/>
             <Shop id='shop'/>
           </div>
 
